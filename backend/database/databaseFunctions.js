4a14d2668916f76ad369f7f8d9b6451a54a03a33
Pyry Ikonen <pyrywilhelmikonen@gmail.com>
2023-11-23 01:02:29 +0200

    Add: dbConfig.js

--- backend/database/databaseFunctions.js
+++ backend/database/databaseFunctions.js
@@ -1,123 +1,123 @@
-const mysql = require('mysql')
-const yup = require('yup')
-const pool = require('../dbConfig') // Use dbConfig.js
+const mysql = require("mysql");
+const yup = require("yup");
+const pool = require("../../dbConfig"); // Use dbConfig.js
 
 const locationSchema = yup.object().shape({
   latitude: yup.number().required(),
   longitude: yup.number().required(),
-})
+});
 
 const idSchema = yup.object().shape({
   id: yup.number().integer().positive().required(),
-})
+});
 
 const validateLocation = (location) => {
-  return locationSchema.validate(location)
-}
+  return locationSchema.validate(location);
+};
 
 const validateId = (id) => {
-  return idSchema.validate({ id })
-}
+  return idSchema.validate({ id });
+};
 
 const queryDatabase = async (query, values = []) => {
   try {
-    console.log('Executing query:', query, 'with values:', values)
-    const [results] = await pool.execute(query, values)
-    console.log('Query results:', results)
-    return results
+    console.log("Executing query:", query, "with values:", values);
+    const [results] = await pool.execute(query, values);
+    console.log("Query results:", results);
+    return results;
   } catch (error) {
-    console.error(`Error in queryDatabase: ${error.message}`)
-    throw error
+    console.error(`Error in queryDatabase: ${error.message}`);
+    throw error;
   }
-}
+};
 
 const findAll = async () => {
   try {
-    const query = 'SELECT * FROM locations'
-    const locations = await queryDatabase(query)
-    return locations
+    const query = "SELECT * FROM locations";
+    const locations = await queryDatabase(query);
+    return locations;
   } catch (error) {
-    console.error(`Error in findAll: ${error.message}`)
-    throw error
+    console.error(`Error in findAll: ${error.message}`);
+    throw error;
   }
-}
+};
 
 const findById = async (id) => {
   try {
-    await validateId(id)
-    const query = 'SELECT * FROM locations WHERE id = ?'
-    const results = await queryDatabase(query, [id])
+    await validateId(id);
+    const query = "SELECT * FROM locations WHERE id = ?";
+    const results = await queryDatabase(query, [id]);
 
     if (results.length === 0) {
-      throw new Error('Location with specified ID not found')
+      throw new Error("Location with specified ID not found");
     }
 
-    return results[0]
+    return results[0];
   } catch (error) {
-    console.error(`Error in findById: ${error.message}`)
-    throw error
+    console.error(`Error in findById: ${error.message}`);
+    throw error;
   }
-}
+};
 
 const deleteById = async (id) => {
   try {
-    await validateId(id)
-    const query = 'DELETE FROM locations WHERE id = ?'
-    const results = await queryDatabase(query, [id])
+    await validateId(id);
+    const query = "DELETE FROM locations WHERE id = ?";
+    const results = await queryDatabase(query, [id]);
 
     if (results.affectedRows === 0) {
-      throw new Error('Location with specified ID not found')
+      throw new Error("Location with specified ID not found");
     }
 
-    return { success: true }
+    return { success: true };
   } catch (error) {
-    console.error(`Error in deleteById: ${error.message}`)
-    throw error
+    console.error(`Error in deleteById: ${error.message}`);
+    throw error;
   }
-}
+};
 
 const save = async (location) => {
   try {
-    const { error } = validateLocation(location)
+    const { error } = validateLocation(location);
 
     if (error) {
-      throw new Error(error.message)
+      throw new Error(error.message);
     }
 
-    const query = 'INSERT INTO locations (latitude, longitude) VALUES (?, ?)'
+    const query = "INSERT INTO locations (latitude, longitude) VALUES (?, ?)";
     const results = await queryDatabase(query, [
       location.latitude,
       location.longitude,
-    ])
+    ]);
 
     const newLocation = {
       id: results.insertId,
       latitude: location.latitude,
       longitude: location.longitude,
-    }
+    };
 
-    return newLocation
+    return newLocation;
   } catch (error) {
-    console.error(`Error in save: ${error.message}`)
-    throw error
+    console.error(`Error in save: ${error.message}`);
+    throw error;
   }
-}
+};
 
 const clearDatabase = async () => {
   try {
-    const query = 'DELETE FROM locations'
-    const results = await queryDatabase(query)
+    const query = "DELETE FROM locations";
+    const results = await queryDatabase(query);
 
     if (results.affectedRows === 0) {
-      throw new Error('No rows found to delete')
+      throw new Error("No rows found to delete");
     }
 
-    return { success: true }
+    return { success: true };
   } catch (error) {
-    console.error(`Error in clearDatabase: ${error.message}`)
-    throw error
+    console.error(`Error in clearDatabase: ${error.message}`);
+    throw error;
   }
-}
+};
 
 module.exports = {
   validateLocation,
@@ -127,4 +127,4 @@ module.exports = {
   deleteById,
   clearDatabase,
   save,
-}
+};
