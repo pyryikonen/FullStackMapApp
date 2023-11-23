8c60f457cc4a1c8d39e747a7ef8da128fdca082d
Pyry Ikonen <pyrywilhelmikonen@gmail.com>
2023-11-23 15:24:10 +0200

    changes

---
+++ backend/dbConfig.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql2/promise"); // Import mysql2 with promise support
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const pool = mysql.createPool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DB,
+  connectionLimit: 10, // Adjust the connection limit based on your application's needs
+});
+
+module.exports = pool;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,13 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    // Ensure assets are copied to the output folder
     assetsInlineLimit: 0,
     assetsDir: "assets",
     rollupOptions: {
-      external: ["/assets/marker-icon-ITd9PylV.png"],
+      external: ["./assets/marker-icon-ITd9PylV.png"], // Adjust the path here
     },
     plugins: [react()],
   },
